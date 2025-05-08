b1012f00cd1f4a115546c56cd186083c3fd8fd38
HWTjac0 <jakub3020.jmy@gmail.com>
2025-05-08 14:34:08 +0200

    styles, fonts

--- src/main/java/com/example/jaqweatherapp/App.java
+++ src/main/java/com/example/jaqweatherapp/App.java
@@ -3,6 +3,7 @@ package com.example.jaqweatherapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,13 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+
+        Font.loadFont(App.class.getResource("/fonts/Inter_24pt-Regular.ttf").toExternalForm(), 24);
+        scene.getStylesheets().add(App.class.getResource("/styles/main.css").toExternalForm());
+
+        stage.setTitle("JacsWeatherApp");
         stage.setScene(scene);
         stage.show();
     }
