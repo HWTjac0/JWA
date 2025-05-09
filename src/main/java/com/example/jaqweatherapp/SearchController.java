659e43b6636ca5500aa718f490f9d3dc9d7c2068
HWTjac0 <jakub3020.jmy@gmail.com>
2025-05-09 10:34:43 +0200

    added toggling search type view

--- src/main/java/com/example/jaqweatherapp/SearchController.java
+++ src/main/java/com/example/jaqweatherapp/SearchController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,6 +20,11 @@ public class SearchController implements Initializable {
     private ToggleButton citySearchButton;
     @FXML
     private ToggleButton coordianteSearchButton;
+    @FXML
+    private HBox citySearchView;
+    @FXML
+    private HBox coordinateSearchView;
+
 
     private enum SearchType { City, Coordinates };
 
@@ -36,16 +42,25 @@ public class SearchController implements Initializable {
                             return;
                         }
                         SearchType searchType = (SearchType) toggleButton.getUserData();
-                        switch (searchType) {
-                            case City:
-                                System.out.println("City");
-                                break;
-                            case Coordinates:
-                                System.out.println("Coordinates");
-                                break;
-                        }
+                        toggleView(searchType);
                     }
                 });
         searchModeGroup.selectToggle(citySearchButton);
+    }
+    private void toggleView(SearchType currentSearchType) {
+        switch (currentSearchType) {
+            case City:
+                coordinateSearchView.setVisible(false);
+                coordinateSearchView.setManaged(false);
+                citySearchView.setVisible(true);
+                citySearchView.setManaged(true);
+                break;
+            case Coordinates:
+                coordinateSearchView.setVisible(true);
+                coordinateSearchView.setManaged(true);
+                citySearchView.setVisible(false);
+                citySearchView.setManaged(false);
+                break;
+        }
     }
 }
