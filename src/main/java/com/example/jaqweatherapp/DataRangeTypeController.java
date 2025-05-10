446e9c8a711cfe58cf6686f9e22dabca1d56e46c
jacmill <jakub3020.jmy@gmail.com>
2025-05-10 15:47:20 +0200

    added data range inputs

---
+++ src/main/java/com/example/jaqweatherapp/DataRangeTypeController.java
@@ -0,0 +1,10 @@
+package com.example.jaqweatherapp;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DataRangeTypeController implements Initializable {
+    public void initialize(URL location, ResourceBundle resources) {}
+}
---
+++ src/main/java/com/example/jaqweatherapp/SearchModel.java
@@ -0,0 +1,4 @@
+package com.example.jaqweatherapp;
+
+public class SearchModel {
+}
