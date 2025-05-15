303b221154c07138dd2fed63231abd48a4cd04d5
jacmill <jakub3020.jmy@gmail.com>
2025-05-16 01:59:56 +0200

    smth

--- src/main/java/com/example/jaqweatherapp/EvaluateController.java
+++ src/main/java/com/example/jaqweatherapp/EvaluateController.java
@@ -5,18 +5,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class EvaluateController implements Initializable {
     @FXML Button fetchButton;
     DataSearchModel dataSearchModel = Context.getInstance().getDataSearchModel();
+    WeatherApiClient weatherApiClient = new WeatherApiClient();
     public void initialize(URL location, ResourceBundle resources) {
         fetchButton.setOnAction(event -> {
+            Map<String, String> params = new HashMap<>();
             for(FilterOption opt : dataSearchModel.filters) {
                 if(!opt.isSet) {
                     continue;
                 }
-                System.out.println(opt.displayName + " " + opt.isSet);
             }
         });
     }
